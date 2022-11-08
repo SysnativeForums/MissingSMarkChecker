38a0e4c22ee81cf2432a109676394bf84817f0cd
MOV-EDX <61068227+MOV-EDX@users.noreply.github.com>
2022-11-08 14:28:27 +0000

    Add project files.

---
+++ Missing s! Mark Checker/Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Win32;
+using Missing_s__Mark_Checker;
+
+Console.Write("Enter the name to load COMPONENTS hive as: ");
+string hive_name = Console.ReadLine();
+Console.Write("Enter the path of the COMPONENTS hive to load: ");
+string path = Console.ReadLine();
+
+RegistryKey deployments = null;
+
+try
+{
+    HiveLoader.GrantPrivileges();
+    int result = HiveLoader.LoadHive(path, hive_name);
+    HiveLoader.RevokePrivileges();
+
+    if (result != 0)
+    {
+        Console.WriteLine("Unable to load the registy hive");
+        return;
+    }
+
+    deployments = HiveLoader.HKLM.OpenSubKey($@"{hive_name}\CanonicalData\Deployments");
+
+    foreach (string deployment_name in deployments.GetSubKeyNames())
+    {
+        RegistryKey deployment = deployments.OpenSubKey(deployment_name, true);
+
+        int smark_count = deployment.GetValueNames().Where(v => v.StartsWith("s!")).Count();
+        int pmark_count = deployment.GetValueNames().Where(v => v.StartsWith("p!")).Count();
+
+        if (pmark_count > smark_count)
+        {
+            foreach (string p_mark in deployment.GetValueNames().Where(v => v.StartsWith("p!")))
+            {
+                object value = deployment.GetValue(p_mark);
+                RegistryValueKind data_type = deployment.GetValueKind(p_mark);
+                string s_mark = p_mark.Replace("p!", "s!");
+
+                deployment.SetValue(s_mark, value, data_type);
+            }
+        }
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine($"An exception has occured: {e.Message}");
+}
+finally
+{
+    HiveLoader.GrantPrivileges();
+    deployments.Close();
+    HiveLoader.HKLM.Close();
+    HiveLoader.UnloadHive(hive_name);
+    HiveLoader.RevokePrivileges();
+}
